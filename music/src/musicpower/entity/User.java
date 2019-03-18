437724f69ad1b004c89bc82054fc0825ecb8bef7
hleif <370419915@qq.com>
2019-03-18 10:42:25 +0800

    付

--- music/src/musicpower/entity/User.java
+++ music/src/musicpower/entity/User.java
@@ -2,145 +2,160 @@ package musicpower.entity;
 
 import java.sql.Timestamp;
 
-
 /**
  * User entity. @author MyEclipse Persistence Tools
  */
 
-public class User  implements java.io.Serializable {
+public class User implements java.io.Serializable {
 
+	// Fields
 
-    // Fields    
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer TUserId;
+	private String nickname;
+	private String username;
+	private String password;
+	private String sex;
+	private String address;
+	private String usyn;
+	private String introduce;
+	private Timestamp time;
+	private String phonenumber;
+	private String zong;  //随机数
 
-     private Integer TUserId;
-     private String nickname;
-     private String username;
-     private String password;
-     private String sex;
-     private String address;
-     private String usyn;
-     private String introduce;
-     private Timestamp time;
-     private String phonenumber;
+	// Constructors
 
-
-    // Constructors
-
-    /** default constructor */
-    public User() {
-    }
+	/** default constructor */
+	public User() {
+	}
 
 	/** minimal constructor */
-    public User(String nickname, String username, String password, String phonenumber) {
-        this.nickname = nickname;
-        this.username = username;
-        this.password = password;
-        this.phonenumber = phonenumber;
-    }
-    
-    /** full constructor */
-    public User(String nickname, String username, String password, String sex, String address, String usyn, String introduce, Timestamp time, String phonenumber) {
-        this.nickname = nickname;
-        this.username = username;
-        this.password = password;
-        this.sex = sex;
-        this.address = address;
-        this.usyn = usyn;
-        this.introduce = introduce;
-        this.time = time;
-        this.phonenumber = phonenumber;
-    }
+	public User(Integer TUserId, String username, String password,
+			String phonenumber) {
+		this.username = username;
+		this.password = password;
+		this.phonenumber = phonenumber;
+		this.TUserId=TUserId;
+	}
 
-   
-    // Property accessors
+	/** full constructor */
+	public User(String nickname, String username, String password, String sex,
+			String address, String usyn, String introduce, Timestamp time,
+			String phonenumber) {
+		this.nickname = nickname;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.address = address;
+		this.usyn = usyn;
+		this.introduce = introduce;
+		this.time = time;
+		this.phonenumber = phonenumber;
+	}
 
-    public Integer getTUserId() {
-        return this.TUserId;
-    }
-    
-    public void setTUserId(Integer TUserId) {
-        this.TUserId = TUserId;
-    }
+	// Property accessors
 
-    public String getNickname() {
-        return this.nickname;
-    }
-    
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
 
-    public String getUsername() {
-        return this.username;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	
+	public Integer getTUserId() {
+		return this.TUserId;
+	}
 
-    public String getPassword() {
-        return this.password;
-    }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getZong() {
+		return zong;
+	}
 
-    public String getSex() {
-        return this.sex;
-    }
-    
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
+	public void setZong(String zong) {
+		this.zong = zong;
+	}
 
-    public String getAddress() {
-        return this.address;
-    }
-    
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public void setTUserId(Integer TUserId) {
+		this.TUserId = TUserId;
+	}
 
-    public String getUsyn() {
-        return this.usyn;
-    }
-    
-    public void setUsyn(String usyn) {
-        this.usyn = usyn;
-    }
+	public String getNickname() {
+		return this.nickname;
+	}
 
-    public String getIntroduce() {
-        return this.introduce;
-    }
-    
-    public void setIntroduce(String introduce) {
-        this.introduce = introduce;
-    }
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
 
-    public Timestamp getTime() {
-        return this.time;
-    }
-    
-    public void setTime(Timestamp time) {
-        this.time = time;
-    }
+	public String getUsername() {
+		return this.username;
+	}
 
-    public String getPhonenumber() {
-        return this.phonenumber;
-    }
-    
-    public void setPhonenumber(String phonenumber) {
-        this.phonenumber = phonenumber;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSex() {
+		return this.sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getUsyn() {
+		return this.usyn;
+	}
+
+	public void setUsyn(String usyn) {
+		this.usyn = usyn;
+	}
+
+	public String getIntroduce() {
+		return this.introduce;
+	}
+
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+
+	public Timestamp getTime() {
+		return this.time;
+	}
+
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}
+
+	public String getPhonenumber() {
+		return this.phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
 
 	@Override
 	public String toString() {
 		return "User [TUserId=" + TUserId + ", nickname=" + nickname
-				+ ", username=" + username + ", password=" + password
-				+ ", sex=" + sex + ", address=" + address + ", usyn=" + usyn
+				+ ", usename=" + username + ", password=" + password + ", sex="
+				+ sex + ", address=" + address + ", usyn=" + usyn
 				+ ", introduce=" + introduce + ", time=" + time
 				+ ", phonenumber=" + phonenumber + "]";
 	}
-   
+
+	
 }
